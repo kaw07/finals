00d6b2bac37e7bb7c6f368d0ba7cafca55aff473
kaw07 <2010022463@ust-ics.mygbiz.com>
2014-11-14 00:29:35 +0800

    a

--- app/src/main/java/com/example/kaw/s1/hymn.java
+++ app/src/main/java/com/example/kaw/s1/hymn.java
@@ -1,4 +1,18 @@
 package com.example.kaw.s1;
 
-public class hymn {
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+
+public class hymn extends ActionBarActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hymn);
+    }
+
+    public void onSubmit(View v) {
+        // closes the activity and returns to first screen
+        this.finish();
+    }
 }
--- app/src/main/java/com/example/kaw/s1/prayer.java
+++ app/src/main/java/com/example/kaw/s1/prayer.java
@@ -1,5 +1,20 @@
 package com.example.kaw.s1;
 
 
-public class prayer {
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+
+public class prayer extends ActionBarActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prayer);
+    }
+
+    public void onSubmit(View v) {
+        // closes the activity and returns to first screen
+        this.finish();
+    }
 }
+
